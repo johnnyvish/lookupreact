8d072d3346b6b2aa239e3397b58c7d52c5283bd7
johnnyvish <johnny.vishnevskiy@gmail.com>
2023-07-25 00:59:02 -0400

    added intro and loading screen

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useRef, Suspense } from "react";
+import React, { useRef, Suspense, useState, useEffect } from "react";
 import { Canvas, useLoader, useFrame } from "@react-three/fiber";
 import { Sphere, PerspectiveCamera, OrbitControls } from "@react-three/drei";
 import { TextureLoader, BackSide } from "three";
@@ -42,7 +42,77 @@ function Stars() {
   );
 }
 
+function Introduction({ onExplore }) {
+  return (
+    <div className="w-screen h-screen bg-black text-white flex justify-center items-center">
+      <button
+        onClick={onExplore}
+        className="bg-white text-black px-8 py-4 border-none cursor-pointer focus:outline-none text-[3rem] font-semibold rounded-full transition duration-200 ease-in-out hover:bg-gray-200"
+      >
+        Explore
+      </button>
+    </div>
+  );
+}
+
+function Loading({ onComplete }) {
+  const [loadProgress, setLoadProgress] = useState(0);
+
+  useEffect(() => {
+    let timer = setInterval(() => {
+      setLoadProgress((prevProgress) => {
+        if (prevProgress < 100) {
+          return prevProgress + 1;
+        } else {
+          clearInterval(timer);
+          onComplete();
+          return prevProgress;
+        }
+      });
+    }, 40); // Speed of loading bar
+
+    return () => {
+      if (timer) {
+        clearInterval(timer);
+      }
+    };
+  }, [onComplete]);
+
+  return (
+    <div className="absolute top-[45%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white flex flex-col items-center justify-center">
+      <object
+        id="svg-logo"
+        className="h-[30vh] w-auto"
+        data="/assets/yellowdot.svg"
+        type="image/svg+xml"
+      />
+      <div>Loading...</div>
+      <div className="w-48 h-4 border border-white bg-black mt-[-8vh]">
+        {" "}
+        {/* Change the value inside the brackets to adjust the overlap */}
+        <div
+          className={`bg-white h-full`}
+          style={{ width: `${loadProgress}%` }}
+        />
+      </div>
+    </div>
+  );
+}
+
 export default function Home() {
+  const [stage, setStage] = useState("intro");
+  const [isLoadingComplete, setLoadingComplete] = useState(false);
+
+  useEffect(() => {
+    if (isLoadingComplete) {
+      setStage("scene");
+    }
+  }, [isLoadingComplete]);
+
+  if (stage === "intro") {
+    return <Introduction onExplore={() => setStage("loading")} />;
+  }
+
   return (
     <div className="w-full h-full overflow-hidden">
       <Canvas style={{ background: "black" }}>
@@ -54,6 +124,11 @@ export default function Home() {
         </Suspense>
         <OrbitControls />
       </Canvas>
+      {stage === "loading" && (
+        <div className="absolute inset-0 bg-black flex justify-center items-center">
+          <Loading onComplete={() => setLoadingComplete(true)} />
+        </div>
+      )}
     </div>
   );
 }
